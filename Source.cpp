2cd72bd8121569e45ef3269e5049da8511852763
Ben Walker <nebior@gmail.com>
2015-06-01 20:19:06 -0400

    ASTAR worse than B&B, favors jumps over scooches, gives diff output
    Doesnt even attempt to handle redundancy

--- Source.cpp
+++ Source.cpp
@@ -7,17 +7,26 @@ struct partialPath{
 		string path;
 		int distanceTraveled;
 	};
+struct partialPathStar{
+		string path;
+		int distanceTraveled;
+		int distanceFromGoal;
+	};
 	bool path_sorter(partialPath const& lhs, partialPath const& rhs){
-			return lhs.distanceTraveled < rhs.distanceTraveled;
-	
+			return lhs.distanceTraveled < rhs.distanceTraveled;	
+	}
+	bool path_sorterStar(partialPathStar const& lhs, partialPathStar const& rhs){
+			return lhs.distanceTraveled < rhs.distanceTraveled;	
+	}
+	bool path_sorterStarFrom(partialPathStar const& lhs, partialPathStar const& rhs){
+			return lhs.distanceFromGoal > rhs.distanceFromGoal;	
 	}
 int main()
 {
-	
-
 	int slength = 0, glength = 0, counter2 = 0, numberOfLines = 0, stringPosition = 0;
-	string input, goal;
-	partialPath stringForPath, in;
+	string input, goal, inputStar;
+	partialPath stringForPath, in; 
+	partialPathStar inStar, stringForPathStar;
 	bool valid = false, movePerformed = false, goalAttained = false;
 	while (valid != true)
 	{
@@ -25,19 +34,18 @@ int main()
 		cin >> input;
 		cout << "Input goal string: ";
 		cin >> goal;
-		char goalArray[1000];		
+		inputStar = input;	
 		slength = input.size();
 		glength = goal.size();
-		if (slength != glength)
+		if (glength != slength)
 		{
 			cout << "Strings are not same length. Re-input strings." << endl;
 		}
 		else
 			valid = true;
-	}
+		}
 	char x[1][1000] , transfer[1][1000], test[1000];
 	deque<partialPath> paths;
-	
 		for (int j = 0; j < 1000; j++)//set entire char array to null
 		{
 			x[0][j] = '\0';
@@ -51,9 +59,13 @@ int main()
 	in.distanceTraveled = 0;
 	paths.push_front(in);
 	stringForPath.distanceTraveled = 0;
+
+	//BRANCH AND BOUND
+
+
+
 	while(goalAttained == false){//while goal not attained
 		while(counter2 < slength){ //run as many times as string is long
-
 for (int j = 0; j < 1000; j++)//check to see it goal is reached
    {
     test[j] =x[0][j];
@@ -61,18 +73,17 @@ for (int j = 0; j < 1000; j++)//check to see it goal is reached
    string tst(test);
    if (tst.substr(0,glength)==goal)
    {
-    cout << tst << endl;
-    system("pause");
-    return 0;
-   }
-			
+    cout <<"Answer according to Branch and Bound: "<< tst << endl;
+	goalAttained = true;
+	counter2 += slength+1;
+   }		
 	if(x[0][counter2] == '(' || x[0][counter2] == '-'){ //Move left if hit (
 		counter2++;
 	}
 	else if(x[0][counter2] == ')'){ //Move to next string when done with current string  ALL CHILDREN BORN: NOW SORT
 		paths.pop_back();
 		//SORT QUEUE BY .distanceTraveled
-		sort(paths.begin(), paths.end(), &path_sorter);
+		sort(paths.begin(), paths.end(), &path_sorter);//sort by distance traveled
 		//SORT ABOVE
 		stringPosition++;//increment position in string array
 		counter2 = 0;
@@ -102,9 +113,9 @@ for (int j = 0; j < 1000; j++)//check to see it goal is reached
 			stringForPath.path+=transfer[0][n];
 			n++;
 			}
-				stringForPath.distanceTraveled+=1;//increment distance, this is likely in wrong place
+				stringForPath.distanceTraveled+=1;//increment distance
 			paths.push_front(stringForPath);
-				numberOfLines++; //increment number of lines
+				numberOfLines++; //REMOVE AT THE END, THIS TRACKS PARENT COUNT FOR DEBUGGING
 					counter2++;
 	}
 	else if(x[0][counter2+2] == '-'){  //if position to the right is -
@@ -126,29 +137,134 @@ for (int j = 0; j < 1000; j++)//check to see it goal is reached
 			j++;
 		}
 			int n = 0;
-			while(transfer[0][n] != '\0'){
+			while(transfer[0][n] != '\0'){//move rest of string to new line
 			stringForPath.path+=transfer[0][n];
 			n++;
 			}
-			stringForPath.distanceTraveled+=1;//increment distance, this is likely in wrong place
+			stringForPath.distanceTraveled+=1;//increment distance
 			paths.push_front(stringForPath);
 			numberOfLines++; //increment number of lines
-				counter2++;
+				counter2++;//next char
 	}
 	else{
+		counter2++;//next char
+	}
+	}
+	}
+
+	
+	//A*
+
+
+	//Spaces * number of chars = # of steps worst case scenario if chars# = '-'# then that = slength - 2 * slength - 1
+	int estimate = ((slength - 2) / 2) * ((slength - 2)/ 2);
+	goalAttained = false;
+	counter2 = 0;
+	inStar.path = inputStar;
+	inStar.distanceTraveled = 0;
+	inStar.distanceFromGoal = estimate;
+	deque<partialPathStar> pathsStar;
+		for (int j = 0; j < 1000; j++)//set entire char array to null
+		{
+			x[0][j] = '\0';
+		}
+	for (int j = 0; j < 1000; j++)//set entire char array to null
+		{
+			transfer[0][j] = '\0';
+		}
+	strcpy_s(x[0], inputStar.c_str());
+	pathsStar.push_front(inStar);
+	stringForPathStar.distanceTraveled = 0;
+	stringForPathStar.distanceFromGoal = estimate;
+
+		while(goalAttained == false){//while goal not attained
+		while(counter2 < slength){ //run as many times as string is long
+for (int j = 0; j < 1000; j++)//check to see it goal is reached
+   {
+    test[j] =x[0][j];
+   }
+   string tstStar(test);
+   if (tstStar.substr(0,glength)==goal)
+   {
+    cout <<"Answer according to A*: "<< tstStar << endl;
+    system("pause");
+    return 0;
+   }		
+	if(x[0][counter2] == '(' || x[0][counter2] == '-'){ //Move left if hit (
 		counter2++;
 	}
+	else if(x[0][counter2] == ')'){ //Move to next string when done with current string  ALL CHILDREN BORN: NOW SORT
+		pathsStar.pop_back();
+		//SORT QUEUE BY .distanceFromGoal (estimated)
+		sort(pathsStar.begin(), pathsStar.end(), &path_sorterStarFrom);//Sort by estimated distance to goal
+		//SORT ABOVE
+		stringPosition++;//REMOVE AT THE END, THIS TRACKS PARENT COUNT FOR DEBUGGING
+		counter2 = 0;
+		inputStar = pathsStar.back().path;//set input to next string 
+		strcpy_s(x[0], inputStar.c_str());//set x[0] to input (the next srting)
 	}
-	}
-	while (true)
-	{
-		if (true)
-		{
-			cout << goal << endl;
-			break;
+	else if(x[0][counter2+1] == '-'){  //if position to the right is -	
+		stringForPathStar = pathsStar.back();
+		stringForPathStar.path.erase();	
+		for(int i = 0;i<1000;i++){
+			transfer[0][i]=0;  //clear out transfer
 		}
-		
+		for(int i = 0;i<slength;i++){
+			transfer[0][i]=x[0][i];  //write newest path to next line
+		}
+		transfer[0][counter2+1]=x[0][counter2];//in new line move char to -
+		transfer[0][counter2] = '-';//in new line put - where char was	
+			int i = slength; //start writing after length of next line
+			int j=0; //new counter
+			while(x[0][j] != '\0'){ //until end of current line
+			transfer[0][i]=x[0][j];//position in next line = info from this line
+			i++;
+			j++;
+		}	
+			int n = 0;
+			while(transfer[0][n] != '\0'){
+			stringForPathStar.path+=transfer[0][n];
+			n++;
+			}
+				stringForPathStar.distanceFromGoal-=1;//decrement estimated distance to goal
+			pathsStar.push_front(stringForPathStar);
+				numberOfLines++; //increment number of lines
+					counter2++;
 	}
+	else if(x[0][counter2+2] == '-'){  //if position to the right is -
+		stringForPathStar = pathsStar.back();
+		stringForPathStar.path.erase();		
+		for(int i = 0;i<1000;i++){
+			transfer[0][i]=0;  //clear out transfer
+		}
+		for(int i = 0;i<slength;i++){
+			transfer[0][i]=x[0][i];  //write newest path to next line
+		}
+		transfer[0][counter2+2]=x[0][counter2];//in new line move char to -
+		transfer[0][counter2] = '-';//in new line put - where char was	
+			int i = slength; //start writing after length of next line
+			int j=0; //new counter
+			while(x[0][j] != '\0'){ //until end of current line
+			transfer[0][i]=x[0][j];//position in next line = info from this line
+			i++;
+			j++;
+		}
+			int n = 0;
+			while(transfer[0][n] != '\0'){//move rest of string to new line
+			stringForPathStar.path+=transfer[0][n];
+			n++;
+			}
+			stringForPathStar.distanceFromGoal-=2;//decrement estimated distance to goal
+			pathsStar.push_front(stringForPathStar);
+			numberOfLines++; //increment number of lines
+				counter2++;//next char
+	}
+	else{
+		counter2++;//next char
+	}
+	}
+	}
+
 	system("pause");
 	return 0;
 }
